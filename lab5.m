b93ed124122bff6beb530cdd8e0a43ee1ca14eab
Mattia <mattia.tinfena@gmail.com>
2024-11-12 15:50:22 +0100

    punto 1 e punto 2 ok, bisogna modificare il codice per fare tutti i print che vuole

--- lab5.m
+++ lab5.m
@@ -1,58 +1,68 @@
 %% Lab5
 
+%% NCC-based segmantation
+
 %Load images
 img1 = imread('ur_c_s_03a_01_L_0376.png');
 img1 = rgb2gray(img1);
 
-
 %Template definition
 T=img1(350:430, 680:780);
-
+T1 = img1(357:417, 544:651);
 
 image_files = {"ur_c_s_03a_01_L_0376.png", "ur_c_s_03a_01_L_0377.png", "ur_c_s_03a_01_L_0378.png","ur_c_s_03a_01_L_0379.png", "ur_c_s_03a_01_L_0380.png", "ur_c_s_03a_01_L_0381.png"};
+
 figure;
-for k = 1:length(image_files)    
+for k = 1:length(image_files)  
+    
     img_k = imread(image_files{k});
     img_k_gray = rgb2gray(img_k);    
-    score_map = normxcorr2(T, img_k_gray);   
-    % imshow(score_map)
-    [ypeak, xpeak] = find(score_map == max(score_map(:)), 1);    
-    yoffSet = ypeak - size(T, 1);
-    xoffSet = xpeak - size(T, 2);    
+    score_map = normxcorr2(T, img_k_gray);
+
+    [xsm, ysm] = size(score_map);
+    [xim, yim, channels] = size(img_k);
+    xdiff = (xsm - xim)/2;
+    ydiff = (ysm - yim)/2;
+    [ypeak, xpeak] = find(score_map == max(score_map(:)), 1);  
+    xoffset = xpeak - xdiff;
+    yoffset = ypeak - ydiff;
+    
+    %print
     subplot(2, 3, k)  
     imshow(img_k);
     hold on;    
-    rectangle('Position', [xoffSet, yoffSet, size(T,2), size(T,1)],'EdgeColor', 'r', 'LineWidth', 2);    
-    plot(xoffSet, yoffSet, 'ro', 'MarkerSize', 2, 'LineWidth', 2);
+    rectangle('Position', [xoffset - (size(T,2) / 2), yoffset - (size(T,1) / 2), size(T,2), size(T,1)],'EdgeColor', 'r', 'LineWidth', 2);    
+    plot(xoffset, yoffset, 'ro', 'MarkerSize', 4, 'LineWidth', 4);
     title(['Detected Position in Image ', num2str(k)]);
     hold off;
 end
 
 
+figure;
+for k = 1:length(image_files)  
+    
+    img_k = imread(image_files{k});
+    img_k_gray = rgb2gray(img_k);    
+    score_map = normxcorr2(T1, img_k_gray);
 
-% 
-% 
-% TM=filter2(T,img1,'same');%two-dimensional correlation
-% % figure,imagesc(TM),colormap gray
-% 
-% T1=T-mean2(T);
-% 
-% TM1=filter2(T1,img1,'same');%two-dimensional correlation
-% % figure,imagesc(TM1),colormap gray
-% 
-% C = normxcorr2(T, img1);%Normalized 2-D cross-correlation
-% figure,imagesc(C),colormap gray
-% 
-% [ypeak, xpeak] = find(C == max(C(:)));
-% yoffSet = ypeak - size(T, 1);
-% xoffSet = xpeak - size(T, 2);
-% 
-% figure;
-% imshow(img1);
-% 
-% hold on;
-% rectangle('Position', [xoffSet, yoffSet, size(T,2), size(T,1)],'EdgeColor', 'r', 'LineWidth', 2);
-% hold off;
+    [xsm, ysm] = size(score_map);
+    [xim, yim, channels] = size(img_k);
+    xdiff = (xsm - xim)/2;
+    ydiff = (ysm - yim)/2;
+    [ypeak, xpeak] = find(score_map == max(score_map(:)), 1);  
+    xoffset = xpeak - xdiff;
+    yoffset = ypeak - ydiff;
+    
+    %print
+    subplot(2, 3, k)  
+    imshow(img_k);
+    hold on;    
+    rectangle('Position', [xoffset - (size(T,2) / 2), yoffset - (size(T,1) / 2), size(T,2), size(T,1)],'EdgeColor', 'r', 'LineWidth', 2);    
+    plot(xoffset, yoffset, 'ro', 'MarkerSize', 4, 'LineWidth', 4);
+    title(['Detected Position in Image ', num2str(k)]);
+    hold off;
+end
+
 
 %% Harris corner detection
 img = double(imread("i235.png"));
