158b99f938b2b7f83bbaa2d636f13fdf9f558bce
Mattia <mattia.tinfena@gmail.com>
2024-11-15 21:04:25 +0100

    code with comparison for report

--- lab5.m
+++ lab5.m
@@ -1,5 +1,5 @@
 %% Lab5
-
+clc;clear;close all;
 %% NCC-based segmantation
 
 %Load images
@@ -9,12 +9,14 @@ img1 = rgb2gray(img1);
 %Template definition
 T=img1(350:430, 680:780);
 T11 = img1(357:417, 544:651);
-T12 = img1(365:410, 555:646);
-T13 = img1(340:430, 530:660); 
+T12=img1(390:400, 575:595);
+T13 = img1(300:450, 500:680); 
 
 % Create a vector t with T11, T12, and T13 inside
 t = {T11, T12, T13};
 times = {0, 0, 0};
+timeNCC = {0, 0, 0, 0, 0, 0};
+timeCBS = {0, 0, 0, 0, 0, 0};
 
 figure 
 subplot(2, 3, 2)
@@ -35,35 +37,55 @@ image_files = {"ur_c_s_03a_01_L_0376.png", "ur_c_s_03a_01_L_0377.png", "ur_c_s_0
 
 figure;
 for k = 1:length(image_files)
-    [xoffset, yoffset] = ncc(image_files{k}, T);
+
+    tic;
+    [xoffset, yoffset, xpeak, ypeak] = ncc(image_files{k}, T);
+    timeNCC{k} = toc;
+    
+    tic;
     [centroid, bounding_box] = cbs(image_files{k});
+    timeCBS{k} = toc;
+   
     subplot(2, 3, k)  
     imshow(image_files{k});
     hold on;    
-    rectangle('Position', [xoffset - (size(T,2) / 2), yoffset - (size(T,1) / 2), size(T,2), size(T,1)],'EdgeColor', 'b', 'LineWidth', 2);    
-    plot(xoffset, yoffset, '*b', 'LineWidth', 2);
+    rectangle('Position', [xoffset , yoffset , size(T,2), size(T,1)],'EdgeColor', 'b', 'LineWidth', 2);    
+    plot(xpeak - size(T,2)/2, ypeak - size(T,1)/2, '*b', 'LineWidth', 2);
     plot(centroid(1), centroid(2),'*g')
     rectangle('Position',bounding_box,'EdgeColor','g', 'LineWidth', 2)
     title(['Detected Position in Image ', num2str(k)]);
     hold off;
 end
+% sgtitle('Normalized Cross-Correlation results to detect the red car');
+sgtitle('Comparison between NCC and CBS to detect the red car');
+NCCavg = mean(cell2mat(timeNCC));
+CBSavg = mean(cell2mat(timeCBS));
 
+disp(['NCC average execution time is: ', num2str(NCCavg), ' seconds']);
+disp(['CBS average execution time is: ', num2str(CBSavg), ' seconds']);
 
 %Dark car
 for i = 1:3
     tic;
     figure;
     for k = 1:length(image_files)
-        [xoffset, yoffset] = ncc(image_files{k}, t{i});
+        [xoffset, yoffset, xpeak, ypeak] = ncc(image_files{k}, t{i});
         subplot(2, 3, k)  
         imshow(image_files{k});
         hold on;    
-        rectangle('Position', [xoffset - (size(t{i},2) / 2), yoffset - (size(t{i},1) / 2), size(t{i},2), size(t{i},1)],'EdgeColor', 'r', 'LineWidth', 2);    
-        plot(xoffset, yoffset, 'r*', 'MarkerSize', 4, 'LineWidth', 4);
+        rectangle('Position', [xoffset , yoffset , size(t{i},2), size(t{i},1)],'EdgeColor', 'r', 'LineWidth', 2);    
+        plot(xpeak- (size(t{i},2) / 2), ypeak - (size(t{i},1) / 2), 'r*', 'MarkerSize', 4, 'LineWidth', 4);
         title(['Detected Position in Image ', num2str(k)]);
         hold off;
     end
     times{i} = toc;
+    if i == 1
+        sgtitle('Normalized Cross-Correlation results to detect the dark car (Normal Window)');
+    elseif i == 2
+        sgtitle('Normalized Cross-Correlation results to detect the dark car (Small Window)');
+    else
+        sgtitle('Normalized Cross-Correlation results to detect the dark car (Large Window)');
+    end
     disp(['execution ', num2str(i), ' time: ',num2str(times{i}), ' seconds']);
 end
 
@@ -105,8 +127,8 @@ for ii=1:rr
 end
 
 figure
-title('R score map');
-imshow(R_map);
+imagesc(R_map);
+title('R score map')
 
 M = max(R_map(:));
 threshold = 0.3 * M;
--- ncc.m
+++ ncc.m
@@ -1,15 +1,15 @@
-function [xoffset,yoffset] = ncc(img, t)
+function [xoffset,yoffset, xpeak, ypeak] = ncc(img, t)
 
     img_k = imread(img);
     img_k_gray = rgb2gray(img_k);    
     score_map = normxcorr2(t, img_k_gray);
 
-    [xsm, ysm] = size(score_map);
-    [xim, yim, channels] = size(img_k);
-    xdiff = (xsm - xim)/2;
-    ydiff = (ysm - yim)/2;
+    % [xsm, ysm] = size(score_map);
+    % [xim, yim, channels] = size(img_k);
+    % xdiff = (xsm - xim)/2;
+    % ydiff = (ysm - yim)/2;
     [ypeak, xpeak] = find(score_map == max(score_map(:)));  
-    xoffset = xpeak - xdiff;
-    yoffset = ypeak - ydiff;
+    xoffset = xpeak - size(t,2);
+    yoffset = ypeak - size(t,1);
     
     end
